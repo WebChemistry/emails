b01de4edf80305f0e002cc2cb3624f65912fccfc
MartkCz <martk494@gmail.com>
2025-03-31 12:46:02 +0200

    chore: tests

--- tests/Token/NinjaMailTesterDoctrineTokenProviderTest.php
+++ tests/Token/NinjaMailTesterDoctrineTokenProviderTest.php
@@ -2,6 +2,7 @@
 
 namespace Tests\Token;
 
+use Symfony\Component\Clock\Test\ClockSensitiveTrait;
 use Symfony\Component\HttpClient\MockHttpClient;
 use Symfony\Component\HttpClient\Response\MockResponse;
 use Symfony\Contracts\HttpClient\HttpClientInterface;
@@ -14,6 +15,7 @@ final class NinjaMailTesterDoctrineTokenProviderTest extends TestCase
 {
 
 	use DatabaseEnvironment;
+	use ClockSensitiveTrait;
 
 	public function testInsert(): void
 	{
@@ -27,6 +29,8 @@ final class NinjaMailTesterDoctrineTokenProviderTest extends TestCase
 
 	public function testUpsert(): void
 	{
+		self::mockTime('2021-01-01 12:00:00');
+
 		$httpClient = new MockHttpClient([
 			MockResponse::fromFile(__DIR__ . '/http/mail_tester_ninja_token.json'),
 			MockResponse::fromFile(__DIR__ . '/http/mail_tester_ninja_token_second.json'),
@@ -34,8 +38,24 @@ final class NinjaMailTesterDoctrineTokenProviderTest extends TestCase
 		$provider = $this->createProvider($httpClient);
 
 		$this->assertSame('Mk5ETGRlQnd3WkZYOGszWXlwQV...WDhQR0pJU0tLa1R4WW5BbXRJVA==', $provider->update());
-		$this->assertSame('second', $provider->update());
+		$this->assertSame([
+			[
+				'id' => 'mail_tester_ninja',
+				'token' => 'Mk5ETGRlQnd3WkZYOGszWXlwQV...WDhQR0pJU0tLa1R4WW5BbXRJVA==',
+				'created' => '2021-01-01 12:00:00',
+			],
+		], $this->databaseSnapshot());
 
+		self::mockTime('2021-01-01 13:00:00');
+
+		$this->assertSame('second', $provider->update());
+		$this->assertSame([
+			[
+				'id' => 'mail_tester_ninja',
+				'token' => 'second',
+				'created' => '2021-01-01 13:00:00',
+			],
+		], $this->databaseSnapshot());
 		$this->assertSame(2, $httpClient->getRequestsCount());
 
 		$provider->reset();
@@ -46,6 +66,18 @@ final class NinjaMailTesterDoctrineTokenProviderTest extends TestCase
 	private function createProvider(HttpClientInterface $httpClient): NinjaMailTesterDoctrineTokenProvider
 	{
 		return new NinjaMailTesterDoctrineTokenProvider('foo', new DefaultTokenRepository($this->connection), $httpClient, strict: true);
+	}
+
+	/**
+	 * @return mixed[]
+	 */
+	private function databaseSnapshot(): array
+	{
+		return $this->connection->createQueryBuilder()
+			->select('id, token, created')
+			->from('tokens')
+			->orderBy('created', 'ASC')
+			->executeQuery()->fetchAllAssociative();
 	}
 
 }
