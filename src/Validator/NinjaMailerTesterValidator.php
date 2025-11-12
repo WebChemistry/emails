fcb408253b8e8fc1461af64f401fdd792dee78a1
MartkCz <martk494@gmail.com>
2025-11-12 18:03:12 +0100

    fix: tokens are deprecated for ninja mailertester

--- src/Validator/NinjaMailerTesterValidator.php
+++ src/Validator/NinjaMailerTesterValidator.php
@@ -2,15 +2,15 @@
 
 namespace WebChemistry\Emails\Validator;
 
-use Symfony\Component\Clock\DatePoint;
+use SensitiveParameter;
 use Symfony\Contracts\HttpClient\HttpClientInterface;
-use WebChemistry\Emails\Token\TokenProvider;
 
 final readonly class NinjaMailerTesterValidator implements EmailValidator
 {
 
 	public function __construct(
-		private TokenProvider $tokenProvider,
+		#[SensitiveParameter]
+		private string $secret,
 		private HttpClientInterface $client,
 	)
 	{
@@ -24,28 +24,11 @@ final readonly class NinjaMailerTesterValidator implements EmailValidator
 		return $this->send($email);
 	}
 
-	private function send(string $email, bool $isRetry = false): ValidationResult
+	private function send(string $email): ValidationResult
 	{
-		$token = $this->tokenProvider->getToken();
-
-		// refresh is required every 24 hours
-		$min = new DatePoint('- 23 hours');
-
-		if ($token->createdAt < $min) {
-			$token = $this->tokenProvider->update();
-		}
-
-		$response = $this->client->request('GET', sprintf('https://happy.mailtester.ninja/ninja?email=%s&token=%s', $email, $token->value));
+		$response = $this->client->request('GET', sprintf('https://happy.mailtester.ninja/ninja?email=%s&key=%s', $email, $this->secret));
 		$payload = $response->toArray();
 		$message = $payload['message'] ?? null;
-
-		if ($message === 'Token Timeout') {
-			if (!$isRetry) {
-				$this->tokenProvider->update();
-			}
-
-			return $this->send($email, true);
-		}
 
 		$ok = in_array($message, ['Accepted', 'Catch-All', 'Limited', 'MX Error', 'Timeout'], true);
 
--- tests/Validator/NinjaMailerTesterValidatorTest.php
+++ tests/Validator/NinjaMailerTesterValidatorTest.php
@@ -2,15 +2,11 @@
 
 namespace Tests\Validator;
 
-use DateTimeImmutable;
 use Symfony\Component\Clock\Test\ClockSensitiveTrait;
 use Symfony\Component\HttpClient\MockHttpClient;
 use Symfony\Component\HttpClient\Response\MockResponse;
 use Tests\DatabaseEnvironment;
 use Tests\TestCase;
-use WebChemistry\Emails\Token\StaticTokenProvider;
-use WebChemistry\Emails\Token\Token;
-use WebChemistry\Emails\Token\TokenProvider;
 use WebChemistry\Emails\Validator\NinjaMailerTesterValidator;
 
 final class NinjaMailerTesterValidatorTest extends TestCase
@@ -23,48 +19,13 @@ final class NinjaMailerTesterValidatorTest extends TestCase
 	{
 		self::mockTime('2021-01-01 12:00:00');
 
-		$tokenProvider = new StaticTokenProvider('static');
 		$httpClient = new MockHttpClient([
 			MockResponse::fromFile(__DIR__ . '/http/ok.json'),
 		]);
-		$validator = new NinjaMailerTesterValidator($tokenProvider, $httpClient);
+		$validator = new NinjaMailerTesterValidator('SECRET', $httpClient);
 
 		$validator->validate('john.doe@example.com');
 
-		$this->assertSame(1, $httpClient->getRequestsCount());
-	}
-
-	public function testRefresh(): void
-	{
-		self::mockTime('2021-01-01 12:00:00');
-
-		$tokenProvider = new class implements TokenProvider {
-
-			public bool $updated = false;
-
-			public function getToken(): Token
-			{
-				return new Token('original', new DateTimeImmutable('2021-01-01 12:00:00'));
-			}
-
-			public function update(): Token
-			{
-				$this->updated = true;
-
-				return new Token('updated');
-			}
-
-		};
-		$httpClient = new MockHttpClient([
-			MockResponse::fromFile(__DIR__ . '/http/ok.json'),
-		]);
-		$validator = new NinjaMailerTesterValidator($tokenProvider, $httpClient);
-
-		self::mockTime('2021-01-02 12:00:00');
-
-		$validator->validate('john.doe@example.com');
-
-		$this->assertTrue($tokenProvider->updated);
 		$this->assertSame(1, $httpClient->getRequestsCount());
 	}
 
